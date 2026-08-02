3d6c9f2d9525ba1b6005280bcb6fa69908b89fab
nneyamot <mohammadnneyamot@gmail.com>
2026-08-02 11:38:42 +0600

    Fix Inngest v4 syntax and update backend dependencies

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,3 +1,51 @@
+// import { Inngest } from "inngest";
+// import { connectDB } from "./db.js";
+// import User from "../models/User.js";
+// import { deleteStreamUser, upsertStreamUser } from "./stream.js";
+
+// export const inngest = new Inngest({ id: "talent-iq" });
+
+// const syncUser = inngest.createFunction(
+//   { id: "sync-user" },
+//   { event: "clerk/user.created" },
+//   async ({ event }) => {
+//     await connectDB();
+
+//     const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
+//     const newUser = {
+//       clerkId: id,
+//       email: email_addresses[0]?.email_address,
+//       name: `${first_name || ""} ${last_name || ""}`,
+//       profileImage: image_url,
+//     };
+
+//     await User.create(newUser);
+
+//     await upsertStreamUser({
+//       id: newUser.clerkId.toString(),
+//       name: newUser.name,
+//       image: newUser.profileImage,
+//     });
+//   }
+// );
+
+// const deleteUserFromDB = inngest.createFunction(
+//   { id: "delete-user-from-db" },
+//   { event: "clerk/user.deleted" },
+//   async ({ event }) => {
+//     await connectDB();
+
+//     const { id } = event.data;
+//     await User.deleteOne({ clerkId: id });
+
+//     await deleteStreamUser(id.toString());
+//   }
+// );
+
+// export const functions = [syncUser, deleteUserFromDB];
+
+
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
@@ -6,8 +54,10 @@ import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 export const inngest = new Inngest({ id: "talent-iq" });
 
 const syncUser = inngest.createFunction(
-  { id: "sync-user" },
-  { event: "clerk/user.created" },
+  { 
+    id: "sync-user",
+    triggers: [{ event: "clerk/user.created" }]
+  },
   async ({ event }) => {
     await connectDB();
 
@@ -31,8 +81,10 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user-from-db" },
-  { event: "clerk/user.deleted" },
+  { 
+    id: "delete-user-from-db",
+    triggers: [{ event: "clerk/user.deleted" }]
+  },
   async ({ event }) => {
     await connectDB();
 
